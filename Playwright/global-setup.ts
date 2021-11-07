412f293e996733b0a58ca253e0153c649423ee3c
EmElkan <eelkan@outlook.com>
2021-11-07 11:14:00 +0000

    tidy up

--- Playwright/global-setup.ts
+++ Playwright/global-setup.ts
@@ -5,18 +5,14 @@ async function globalSetup(config: FullConfig) {
 
     const standardUser = await browser.newPage();
     await standardUser.goto('https://www.saucedemo.com/');
-    await standardUser.click('[data-test="username"]');
     await standardUser.fill('[data-test="username"]', 'standard_user');
-    await standardUser.click('[data-test="password"]');
     await standardUser.fill('[data-test="password"]', 'secret_sauce');
     await standardUser.click('[data-test="login-button"]');
     await standardUser.context().storageState({ path: 'standardUserStorageState.json' });
 
     const problemUser = await browser.newPage();
     await problemUser.goto('https://www.saucedemo.com/');
-    await problemUser.click('[data-test="username"]');
     await problemUser.fill('[data-test="username"]', 'problem_user');
-    await problemUser.click('[data-test="password"]');
     await problemUser.fill('[data-test="password"]', 'secret_sauce');
     await problemUser.click('[data-test="login-button"]');
     await problemUser.context().storageState({ path: 'problemUserStorageState.json' });
--- Playwright/tests/swag-labs.spec.ts
+++ Playwright/tests/swag-labs.spec.ts
@@ -4,23 +4,31 @@ test.describe.parallel('Admin tests', () => {
     test.use({ storageState: 'standardUserStorageState.json' });
 
     test('view inventory', async ({ page }) => {
+        const title = page.locator('.inventory_details_desc_container');
         await page.goto('https://www.saucedemo.com/inventory.html');
         await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
         await page.click('text=Sauce Labs Bolt T-Shirt');
         await expect(page).toHaveURL('https://www.saucedemo.com/inventory-item.html?id=1');
-        await page.click('text=Sauce Labs Bolt T-Shirt');
+        await expect(title).toHaveText(/Sauce Labs Bolt T-Shirt/);
+    });
+
+    test('back to products', async ({ page }) => {
+        await page.goto('https://www.saucedemo.com/inventory-item.html?id=1');
+        await page.click('[data-test="back-to-products"]');
+        await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
     });
 });
 
-test.describe.parallel('Problem tests', () => {
+test.describe('Problem tests', () => {
     test.use({ storageState: 'problemUserStorageState.json' });
     test.fixme();
 
     test('problem view inventory', async ({ page }) => {
+        const title = page.locator('.inventory_details_desc_container');
         await page.goto('https://www.saucedemo.com/inventory.html');
         await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
         await page.click('text=Sauce Labs Bolt T-Shirt');
         await expect(page).toHaveURL('https://www.saucedemo.com/inventory-item.html?id=1');
-        await page.click('text=Sauce Labs Bolt T-Shirt');
+        await expect(title).toHaveText(/Sauce Labs Bolt T-Shirt/);
     });
 });
